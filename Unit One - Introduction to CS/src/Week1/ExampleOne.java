b47eda5f6ad22ebe7d2be7a78e97ab5e0116b7c9
StefyfreshSchool <sesposto@bayviewglen.ca>
2021-09-09 09:24:48 -0400

    Initial Commit for Unit 1

---
+++ Unit One - Introduction to CS/src/Week1/ExampleOne.java
@@ -0,0 +1,15 @@
+package Week1;
+
+//each file is a class
+public class ExampleOne {
+
+    public static void main(String[] args) {
+        //main function runs at start
+        System.out.println("Welocme to ICS 3U - AP");        
+        System.out.println("Welocme to ICS 3U - AP");
+        System.out.println("Welocme to ICS 3U - AP");
+        System.out.println("Welocme to ICS 3U - AP");
+        System.out.println("Welocme to ICS 3U - AP");
+        System.out.println("Welocme to ICS 3U - AP");
+    }
+}
