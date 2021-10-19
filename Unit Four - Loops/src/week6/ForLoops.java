569f13a986084827beb61d49fbecc77a29abfd77
StefyfreshSchool <sesposto@bayviewglen.ca>
2021-10-19 09:46:08 -0400

    For Loops

---
+++ Unit Four - Loops/src/week6/ForLoops.java
@@ -0,0 +1,37 @@
+package week6;
+
+public class ForLoops {
+    public static void main(String[] args) {
+        whileLoop();
+        forLoop();
+        forLoop2();
+
+    }   
+
+    private static void forLoop2() {
+        int sum = 0;
+        for (int i = 0; i <= 100; i+= 2){
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+
+    private static void forLoop() {
+        int sum = 0;
+        for (int i = 0; i <= 100; i++){
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+
+    private static void whileLoop() {
+        int i = 0;
+        int sum = 0;
+        while (i <= 100){
+            sum += i;
+            i++;
+        }
+        System.out.println(sum);
+    }
+    
+}
